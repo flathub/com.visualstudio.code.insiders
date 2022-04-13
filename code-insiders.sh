ea991e57e9be26c29faa8ce70af5e4fd22fc7278
Giovanni Grieco <giovanni@grieco.dev>
2022-04-13 15:09:52 +0200

    Align code to com.visualstudio.code
    Flatpak com.visualstudio.code contains updates that this repository would also benefit.

--- code-insiders.sh
+++ code-insiders.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+shopt -s nullglob
 
 FIRST_RUN="${XDG_CONFIG_HOME}/flatpak-vscode-first-run"
 
@@ -12,6 +13,30 @@ if [ ! -f ${FIRST_RUN} ]; then
   WARNING_FILE="/app/share/vscode-insiders/flatpak-warning.txt"
   touch ${FIRST_RUN}
 fi
+
+PYTHON_SITEDIR=$(python3 <<EOFPYTHON
+import os
+import site
+print(os.path.relpath(site.getusersitepackages(), site.getuserbase()))
+EOFPYTHON
+)
+
+for tool_dir in /app/tools/*; do
+  tool_bindir=$tool_dir/bin
+  if [ -d "$tool_bindir" ]; then
+    msg "Adding $tool_bindir to PATH"
+    export PATH=$PATH:$tool_bindir
+  fi
+  tool_pythondir=$tool_dir/$PYTHON_SITEDIR
+  if [ -d "$tool_pythondir" ]; then
+    msg "Adding $tool_pythondir to PYTHONPATH"
+    if [ -z "$PYTHONPATH" ]; then
+      export PYTHONPATH=$tool_pythondir
+    else
+      export PYTHONPATH=$PYTHONPATH:$tool_pythondir
+    fi
+  fi
+done
 
 if [ "$FLATPAK_ENABLE_SDK_EXT" = "*" ]; then
   SDK=()
@@ -35,5 +60,7 @@ for i in "${SDK[@]}"; do
   fi
 done
 
-exec env PATH="${PATH}:${XDG_DATA_HOME}/node_modules/bin" \
-  /app/extra/vscode-insiders/bin/code-insiders --extensions-dir=${XDG_DATA_HOME}/vscode-insiders/extensions "$@" ${WARNING_FILE}
+exec env ELECTRON_RUN_AS_NODE=1 PATH="${PATH}:${XDG_DATA_HOME}/node_modules/bin" \
+  /app/bin/zypak-wrapper.sh /app/extra/vscode-insiders/code-insiders /app/extra/vscode-insiders/resources/app/out/cli.js \
+  --ms-enable-electron-run-as-node --extensions-dir=${XDG_DATA_HOME}/vscode-insiders/extensions \
+  "$@" ${WARNING_FILE}
